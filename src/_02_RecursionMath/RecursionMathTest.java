094345bfcd6d429872931d5d80a0301694be0d56
Meme1sh_ <60296686+The-GitHub-Paradox@users.noreply.github.com>
2021-03-13 16:06:59 -0800

    e
    e

--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -10,20 +10,27 @@ class RecursionMathTest {
     @Test
     public void testMultiplication() {
         assertEquals(12, RecursionMath.recursiveMultiplication(3, 4));
-        
-        // Add more JUnit tests like the one above to test your method
+        assertEquals(20, RecursionMath.recursiveMultiplication(4, 5));
+        assertEquals(30, RecursionMath.recursiveMultiplication(5, 6));
+        assertEquals(42, RecursionMath.recursiveMultiplication(6, 7));
         
     }
 
     @Test
     public void testDivision() {
-        // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(1, RecursionMath.recursiveDivision(3, 3));
+        assertEquals(2, RecursionMath.recursiveDivision(4, 2));
+        assertEquals(5, RecursionMath.recursiveDivision(5, 1));
+        assertEquals(10, RecursionMath.recursiveDivision(20, 2));
+        assertEquals(20, RecursionMath.recursiveDivision(60, 3));
     }
 
     @Test 
     public void testPower() {
-        // Add JUnit tests to test your method
-        fail("Write some tests!");
+    	assertEquals(1, RecursionMath.recursivePower(1, 1));
+    	assertEquals(4, RecursionMath.recursivePower(2, 2));
+    	assertEquals(9, RecursionMath.recursivePower(3, 3));
+    	assertEquals(16, RecursionMath.recursivePower(4, 4));
+    	assertEquals(25, RecursionMath.recursivePower(5, 5));
     }
 }
